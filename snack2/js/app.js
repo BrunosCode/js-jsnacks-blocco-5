db836e26c24dedfef108816575e7c92ec1d6a8c1
brunEdo <edobruno.m@gmail.com>
2021-09-08 11:48:05 +0200

    snack2

---
+++ snack2/js/app.js
@@ -0,0 +1,39 @@
+// Make array of object courgettes
+let courgettes = [];
+
+class Courgette {
+    constructor(variety, weight, length) {
+        this.variety = variety;
+        this.weight = weight;
+        this.length = length
+    }
+    presentation() {
+        return `This courgette is a ${this.variety}, it is ${this.weight}g and ${this.length}cm`;
+    }
+}
+
+const getRndNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// add random courgette
+for( let i = 0; i < 10; i++){
+    courgettes.push(new Courgette(getRndNumber(1,5), getRndNumber(70,300), getRndNumber(10,80)));
+}
+
+let smallCourgettes = [];
+let longCourgettes = [];
+
+// print all the courgette
+for(let i = 0; i < courgettes.length; i++){
+    console.log(`${i+1}. ${courgettes[i].presentation()}`);
+    if ( courgettes[i].length > 15 ) {
+        longCourgettes.push(courgettes[i]);
+    } else {
+        smallCourgettes.push(courgettes[i]);
+    }
+}
+
+console.log(`smallCourgettes ${smallCourgettes}`);
+console.log(`longsCourgettes ${longsCourgettes}`);
+
