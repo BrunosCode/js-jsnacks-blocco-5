88108ddc80ad67c09c028f370374eb7b175a2c5d
brunEdo <edobruno.m@gmail.com>
2021-09-08 13:11:02 +0200

    snack4 length control

--- snack4/js/app.js
+++ snack4/js/app.js
@@ -2,14 +2,31 @@
 // elementi) prendendo alternativamente gli elementi da uno e dall’altro
 // es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 
-const array1 = ["a", "b", "c", "d", "e"];
+const array1 = ["a", "b", "c", "d", "e", "f"];
 const array2 = [1,2,3,4,5];
 
 const mergeArrays = (array1, array2) => {
     let mergedArray = [];
-    for(let i = 0; i < array1.length; i++) {
-        mergedArray.push(array1[i]);
-        mergedArray.push(array2[i]);
+    if (array1.length === array2.length) {
+        for (let i = 0, u = 0; i < array1.length; i++, u++) {
+            mergedArray.push(array1[i]);
+            mergedArray.push(array2[u]);
+        }
+    } else if (array1.length > array2.length) {
+        for (let i = 0, u = 0; i < array1.length; i++, u++) {
+            mergedArray.push(array1[i]);
+            if ( u < array2.length - 1) {
+                mergedArray.push(array2[u]);
+            }
+        }
+    } else if (array1.length < array2.length) {
+        for (let i = 0, u = 0; i < array1.length; i++, u++) {
+
+            if ( i < array1.length - 1) {
+                mergedArray.push(array1[i]);
+            }
+            mergedArray.push(array2[u]);
+        }
     }
     return mergedArray;
 }
