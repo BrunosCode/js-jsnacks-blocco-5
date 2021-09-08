f75cb0398a296898641b929987b93cf4b510d784
brunEdo <edobruno.m@gmail.com>
2021-09-08 12:57:05 +0200

    snack4 function

--- snack4/js/app.js
+++ snack4/js/app.js
@@ -1,9 +1,16 @@
-let array1 = ["a", "b", "c", "d", "e"];
-let array2 = [1,2,3,4,5];
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di
+// elementi) prendendo alternativamente gli elementi da uno e dall’altro
+// es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 
-let mergedArray = [];
-for(let i = 0; i < array1.length; i++) {
-    mergedArray.push(array1[i]);
-    mergedArray.push(array2[i]);
+const array1 = ["a", "b", "c", "d", "e"];
+const array2 = [1,2,3,4,5];
+
+const mergeArrays = (array1, array2) => {
+    let mergedArray = [];
+    for(let i = 0; i < array1.length; i++) {
+        mergedArray.push(array1[i]);
+        mergedArray.push(array2[i]);
+    }
+    return mergedArray;
 }
-console.log(mergedArray);
+console.log(mergeArrays(array1, array2));
