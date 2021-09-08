d19ccfed1af758e75758c831f18b53f9e9402e5f
brunEdo <edobruno.m@gmail.com>
2021-09-08 12:12:02 +0200

    snack2 two arrays weight

--- snack2/js/app.js
+++ snack2/js/app.js
@@ -1,11 +1,15 @@
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
 // Make array of object courgettes
 let courgettes = [];
 
 class Courgette {
-    constructor(variety, weight, length1) {
+    constructor(variety, weight, mineLength) {
         this.variety = variety;
         this.weight = weight;
-        this.length1 = length1
+        this.mineLength = mineLength
     }
     presentation() {
         return `This courgette is a ${this.variety}, it is ${this.weight}g and ${this.length}cm`;
@@ -20,14 +24,23 @@ const getRndNumber = (min, max) => {
 for( let i = 0; i < 10; i++){
     courgettes.push(new Courgette(getRndNumber(1,5), getRndNumber(70,300), getRndNumber(10,50)));
 }
-console.log(courgettes);
-let smallCourgettes = [];
-let longCourgettes = [];
 
 // print all the courgette
 for(let i = 0; i < courgettes.length; i++){
-    // console.log(`${i+1}. ${courgettes[i].presentation()}`);
-    if ( courgettes[i].length1 > 15 ) {
+    console.log(`${i+1}. ${courgettes[i].presentation()}`);
+}
+
+// sum of all courgette weights
+let weightsSum = 0;
+for( let courgette of courgettes){
+    weightsSum += courgette.weight;
+}
+console.log(`the sum of courgette weights is ${weightsSum}g`);
+
+let smallCourgettes = [];
+let longCourgettes = [];
+for(let i = 0; i < courgettes.length; i++){
+    if ( courgettes[i].mineLength > 15 ) {
         longCourgettes.push(courgettes[i]);
     } else {
         smallCourgettes.push(courgettes[i]);
@@ -37,3 +50,15 @@ for(let i = 0; i < courgettes.length; i++){
 console.log("small", smallCourgettes);
 console.log("long", longCourgettes);
 
+// sum of small and long courgette weights
+let weightsSmallSum = 0;
+for( let courgette of smallCourgettes){
+    weightsSmallSum += courgette.weight;
+}
+console.log(`the sum of small courgette weights is ${weightsSmallSum}g`);
+
+let weightsLongSum = 0;
+for( let courgette of longCourgettes){
+    weightsLongSum += courgette.weight;
+}
+console.log(`the sum of long courgette weights is ${weightsLongSum}g`);
